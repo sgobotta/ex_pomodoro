61a5bf6de6d61025fda011ed04363552c0ccb4fb
Santiago Botta <sgobotta@gmail.com>
2022-11-22 01:28:07 -0300

    Implement and test a pomodoro module

---
+++ lib/ex_pomodoro/pomodoro.ex
@@ -0,0 +1,78 @@
+defmodule ExPomodoro.Pomodoro do
+  @moduledoc """
+  This module is repsonsible for defining the domain model for the Pomodoro
+  technique.
+  """
+
+  @exercise_duration 25
+  @break_duration 5
+  @rounds 4
+
+  @enforce_keys [:id]
+  defstruct [
+    :id,
+    activity: :exercise,
+    exercise_duration: @exercise_duration,
+    break_duration: @break_duration,
+    rounds: 4
+  ]
+
+  @type t :: %__MODULE__{}
+  @type pomodoro_activity :: :exercise | :break
+  @type pomodoro_opts :: [
+          exercise_duration: non_neg_integer(),
+          break_duration: non_neg_integer(),
+          rounds: non_neg_integer()
+        ]
+
+  @doc """
+  Given an id and a keyword of options, returns a new #{__MODULE__} struct.
+  The options can redefine the following fields, otherwise the default values
+  will be used:
+  * `exercise_duration`: the amount of time intended to spend on task
+  completion, in minutes (default: `25`).
+  * `break_duration`: the amount of time the break lasts, in minutes (default:
+  `5`).
+  * `rounds`: the number of rounds until the pomodoro finishes (default: `4`).
+
+  """
+  @spec new(String.t(), pomodoro_opts()) :: t()
+  def new(id, opts \\ []) do
+    parsed_opts = parse_opts(opts)
+
+    %__MODULE__{
+      id: id,
+      activity: :exercise,
+      exercise_duration:
+        :timer.minutes(Keyword.fetch!(parsed_opts, :exercise_duration)),
+      break_duration:
+        :timer.minutes(Keyword.fetch!(parsed_opts, :break_duration)),
+      rounds: Keyword.fetch!(parsed_opts, :rounds)
+    }
+  end
+
+  @doc """
+  The default duration of the exercise.
+  """
+  @spec default_exercise_duration() :: non_neg_integer()
+  def default_exercise_duration, do: @exercise_duration
+
+  @doc """
+  The default duration of breaks.
+  """
+  @spec default_break_duration() :: non_neg_integer()
+  def default_break_duration, do: @break_duration
+
+  @doc """
+  The default rounds number.
+  """
+  @spec default_rounds() :: non_neg_integer()
+  def default_rounds, do: @rounds
+
+  defp parse_opts(opts) do
+    opts
+    |> Keyword.put_new(:exercise_duration, default_exercise_duration())
+    |> Keyword.put_new(:break_duration, default_break_duration())
+    |> Keyword.put_new(:rounds, default_rounds())
+  end
+end
---
+++ test/ex_pomodoro/pomodoro_test.exs
@@ -0,0 +1,71 @@
+defmodule ExPomodoro.PomodoroTest do
+  @moduledoc false
+
+  use ExUnit.Case
+
+  alias ExPomodoro.Fixtures.PomodoroFixtures
+  alias ExPomodoro.Pomodoro
+
+  describe "Pomodoro.new/2" do
+    setup do
+      %{
+        id: id
+      } = PomodoroFixtures.valid_attrs()
+
+      %Pomodoro{} = pomodoro = Pomodoro.new(id, [])
+
+      %{pomodoro: pomodoro}
+    end
+
+    @tag :wip
+    test "returns a #{Pomodoro} struct" do
+      %{
+        id: id,
+        break_duration: break_duration,
+        exercise_duration: exercise_duration,
+        rounds: rounds
+      } = PomodoroFixtures.valid_attrs()
+
+      expected_break_duration = :timer.minutes(break_duration)
+      expected_exercise_duration = :timer.minutes(exercise_duration)
+
+      %Pomodoro{
+        id: ^id,
+        activity: :exercise,
+        break_duration: ^expected_break_duration,
+        exercise_duration: ^expected_exercise_duration,
+        rounds: ^rounds
+      } =
+        do_new(id,
+          exercise_duration: exercise_duration,
+          break_duration: break_duration,
+          rounds: rounds
+        )
+    end
+
+    test "returns a #{Pomodoro} struct with default values" do
+      %{
+        id: id
+      } = PomodoroFixtures.valid_attrs()
+
+      expected_break_duration =
+        :timer.minutes(Pomodoro.default_break_duration())
+
+      expected_exercise_duration =
+        :timer.minutes(Pomodoro.default_exercise_duration())
+
+      expected_rounds = Pomodoro.default_rounds()
+
+      %Pomodoro{
+        id: ^id,
+        activity: :exercise,
+        break_duration: ^expected_break_duration,
+        exercise_duration: ^expected_exercise_duration,
+        rounds: ^expected_rounds
+      } = do_new(id)
+    end
+  end
+
+  defp do_new(id), do: Pomodoro.new(id)
+  defp do_new(id, opts), do: Pomodoro.new(id, opts)
+end
