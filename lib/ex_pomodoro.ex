f4afc9a5dd818ee5f4767ca664792cb30f8a8be7
Santiago Botta <sgobotta@gmail.com>
2022-11-23 00:55:51 -0300

    Add comment

--- lib/ex_pomodoro.ex
+++ lib/ex_pomodoro.ex
@@ -3,6 +3,12 @@ defmodule ExPomodoro do
   Documentation for `ExPomodoro`.
   """
 
+  @doc """
+  Returns the #{ExPomodoro} child spec. It is intended for appliations to
+  add an #{ExPomodoro} child spec to their application trees to have an
+  #{ExPomodoro.Supervisor} started before interacting with the rest of the
+  #{ExPomodoro} commands.
+  """
   @spec child_spec(keyword) :: Supervisor.child_spec()
   defdelegate child_spec(options), to: ExPomodoro.Supervisor
 end
