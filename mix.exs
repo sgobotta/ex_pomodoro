e13e693e469bd53b9392679aacaaeb066e790a17
Santiago Botta <sgobotta@gmail.com>
2022-12-03 11:50:09 -0300

    Updates required Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExPomodoro.MixProject do
     [
       app: :ex_pomodoro,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
